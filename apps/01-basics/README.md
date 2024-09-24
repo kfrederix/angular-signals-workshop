4312a78d19471786110ebf2a7a2edab102777fd5
Karel Frederix <karelfrederix@gmail.com>
2024-09-24 12:31:18 +0200

    add first app: 01-basics

---
+++ apps/01-basics/README.md
@@ -0,0 +1,13 @@
+# Basics
+
+## Run app
+
+```bash
+pnpm start basics
+```
+
+## Instructions
+
+This application shows the current time. Unfortunately, it is broken 😢 The displayed time on the page should always be up-to-date, but with current implementation it does not seem to work. The time is correct when we refresh the page, but it remains static on the page and does not update 🤔
+
+Now it's up to you to fix it. Good luck!
---
+++ apps/01-basics/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-root',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <main class="p-6">
+      <h1 class="text-xl font-bold mb-4">01 - Basics</h1>
+      <div class="flex gap-4 items-center">
+        <span>The current time is:</span>
+        <pre class="text-xl">{{ currentTime }}</pre>
+      </div>
+    </main>
+  `,
+})
+export class AppComponent {
+  currentTime = this.getCurrentTime();
+
+  constructor() {
+    setInterval(() => this.updateTime(), 100);
+  }
+
+  private updateTime(): void {
+    this.currentTime = this.getCurrentTime();
+  }
+
+  private getCurrentTime(): string {
+    return new Date().toLocaleTimeString('nl-BE');
+  }
+}
