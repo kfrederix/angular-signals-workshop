d208cf0462b63c6aad0495df5941d303da9a5afd
Karel Frederix <karelfrederix@gmail.com>
2024-09-28 09:04:16 +0200

    solution 1

--- apps/01-basics/src/app/app.component.ts
+++ apps/01-basics/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   standalone: true,
@@ -9,20 +9,20 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
       <h1 class="text-xl font-bold mb-4">01 - Basics</h1>
       <div class="flex gap-4 items-center">
         <span>The current time is:</span>
-        <pre class="text-xl">{{ currentTime }}</pre>
+        <pre class="text-xl">{{ currentTime() }}</pre>
       </div>
     </main>
   `,
 })
 export class AppComponent {
-  currentTime = this.getCurrentTime();
+  currentTime = signal(this.getCurrentTime());
 
   constructor() {
     setInterval(() => this.updateTime(), 100);
   }
 
   private updateTime(): void {
-    this.currentTime = this.getCurrentTime();
+    this.currentTime.set(this.getCurrentTime());
   }
 
   private getCurrentTime(): string {
