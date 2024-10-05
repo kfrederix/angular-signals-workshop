25806b132ef554aff94a66479feda1996b73e12d
Karel Frederix <karelfrederix@gmail.com>
2024-10-05 15:56:32 +0200

    exercise 5: alternative solution

--- apps/05-fun-with-signals/src/app/pokemon-cards-search/cards-search.component.ts
+++ apps/05-fun-with-signals/src/app/pokemon-cards-search/cards-search.component.ts
@@ -1,13 +1,10 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { ActivatedRoute, Router } from '@angular/router';
-import { derivedAsync } from 'ngxtension/derived-async';
 import { injectQueryParams } from 'ngxtension/inject-query-params';
-import { of, type Observable } from 'rxjs';
-import { catchError, map, startWith } from 'rxjs/operators';
-import type { ApiCallState } from './api-call-state.interface';
+import { switchMap, tap } from 'rxjs/operators';
 import { PokemonApiService } from './api/pokemon-api.service';
 import { PokemonCardsGridComponent } from './cards-grid.component';
-import type { PokemonCard } from './entities/pokemon-card.interface';
 import { LoadingIndicatorComponent } from './loading-indicator.component';
 import { PokemonSearchBarComponent } from './search-bar.component';
 
@@ -20,17 +17,10 @@ import { PokemonSearchBarComponent } from './search-bar.component';
     <div class="mt-6 flex flex-col gap-4">
       <pokemon-search-bar [initialSearchQuery]="searchParam()" (search)="onSearch($event)" />
 
-      @let apiState = searchApiState();
-      @switch (apiState.status) {
-        @case ('loading') {
-          <div class="p-2"><loading-indicator /></div>
-        }
-        @case ('loaded') {
-          <pokemon-cards-grid [cards]="apiState.result ?? []" />
-        }
-        @case ('error') {
-          <p>Oops, something went wrong 💥. Please try again in a minute.</p>
-        }
+      @if (isLoading()) {
+        <div class="p-2"><loading-indicator /></div>
+      } @else {
+        <pokemon-cards-grid [cards]="cards() ?? []" />
       }
     </div>
   `,
@@ -45,26 +35,19 @@ export class PokemonCardsSearchComponent {
   // see: https://ngxtension.netlify.app/utilities/injectors/inject-query-params/
   protected readonly searchParam = injectQueryParams('search');
 
+  protected isLoading = signal(false);
+
   // derived state
-  protected readonly searchApiState = derivedAsync(() => this.fetchCardsWithApiState(this.searchParam() ?? ''), { requireSync: true });
+  protected readonly cards = toSignal(
+    toObservable(this.searchParam).pipe(
+      tap(() => this.isLoading.set(true)),
+      switchMap((searchQuery) => this.pokemonApiService.searchCardsByName(searchQuery ?? '')),
+      tap(() => this.isLoading.set(false)),
+    ),
+  );
 
   onSearch(searchValue: string): void {
     this.patchUrlSearchParam(searchValue);
-  }
-
-  /**
-   * Search for card, with ApiCallState included
-   *
-   * NOTE: This will always emit 2 values:
-   *  - First it will emit { status: 'loading', result: [] } IMMEDIATELY (thanks to the startWith)
-   *  - Then, it will emit the real result with status 'loaded' when api call finished (or an error)
-   */
-  private fetchCardsWithApiState(searchQuery: string): Observable<ApiCallState<PokemonCard[]>> {
-    return this.pokemonApiService.searchCardsByName(searchQuery).pipe(
-      map((cards) => ({ status: 'loaded' as const, result: cards })),
-      startWith({ status: 'loading' as const, result: [] }),
-      catchError((err) => of({ status: 'error' as const, error: err })),
-    );
   }
 
   /**
